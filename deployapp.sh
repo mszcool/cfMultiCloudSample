079f3282735c0dd9eacb29c194b92585c5d6f239
Mario Szpuszta <marioszp@microsoft.com>
2016-09-28 12:08:52 +0200

    Adding sample app and new figure

---
+++ deployapp.sh
@@ -0,0 +1,50 @@
+#
+# First read all required parameters
+#
+trafficMgrDomain=$1
+azureCfPublicIp=$2
+pivotalOrg=$3
+pivotalSpace=$4
+azureOrg=$5
+azureSpace=$6
+
+$azureCfApiEndpoint="api.$azureCfPublicIp.xip.io"
+$pivotalApiEndpoint="api.run.pivotal.io"
+
+echo "############################################################"
+echo "            Setting up apps on Azure and Pivotal            "
+echo "!! Press CTRL+C within 30 sec if the below is not correct !!"
+echo "############################################################"
+echo "- Traffic Manager Domain: $trafficMgrDomain"
+echo "- Azure API endpoint: $azureCfApiEndpoint"
+echo "  - Azure CF organization: $azureOrg"
+echo "  - Azure CF space: $azureSpace"
+echo "- Pivotal API endpoint: $pivotalApiEndpoint"
+echo "  - Pivotal CF organization: $pivotalOrg"
+echo "  - Pivotal CF space: $pivotalSpace"
+sleep 30
+
+echo ""
+echo "### Pivotal setup ###"
+cf login -a $pivotalApiEndpoint
+cf target -o $pivotalOrg -s $pivotalSpace
+cf push -f ./sampleapp/manifest.yml -p ./sampleapp
+cf create-domain $pivotalOrg $trafficMgrDomain
+cf create-route $pivotalSpace $trafficMgrDomain
+cf map-route multicloudapp $trafficMgrDomain
+cf set-env multicloudapp REGION "Pivotal Cloud"
+
+echo ""
+echo "### Azure setup ###"
+cf login -a $azureCfApiEndpoint
+cf target -o $azureOrg -s $azureSpace
+cf push -f ./sampleapp/manifest.yml -p ./sampleapp
+cf create-domain $azureOrg $trafficMgrDomain
+cf create-route $azureSpace $trafficMgrDomain
+cf map-route multicloudapp $trafficMgrDomain
+cf set-env multicloudapp REGION "Microsoft Azure"
+
+echo ""
+echo "### Finished ###"
+echo "Navigate to $trafficMgrDomain to test your app."
+echo "Try cf stop on one app, wait a while (300s max. DNS TTL), and try again"
---
+++ sampleapp/config.ru
@@ -0,0 +1,29 @@
+require "sinatra/base"
+require "json"
+
+class Web < Sinatra::Base
+
+  get "/" do
+    raise "Please define environment variable 'REGION' with the cloud region you're running in!" unless ENV.has_key?("REGION")
+
+    region = ENV.fetch("REGION")
+    instance = ENV.fetch("CF_INSTANCE_INDEX", 0)
+    addr = ENV.fetch("CF_INSTANCE_ADDR", "127.0.0.1")
+
+    %{
+      <h1>I am running in #{REGION}</h1>
+      <hr />
+      <h2>I am app instance #{instance}</h2>
+      <h2>I am running at #{addr}</h2>
+      <hr />
+    }
+  end
+
+  get "/env.json" do
+    content_type "application/json"
+    JSON.pretty_generate(ENV.to_h)
+  end
+
+end
+
+run Web.new
